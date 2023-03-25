4d279ef88c3b830f45873370b71bdd4aee161054
MavundlaBK <mavundlabj@gmail.com>
2023-03-25 01:43:05 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,11 @@
 #include "main.h"
+
 /**
- * print_alphabet_x10 - prints 10 times the alphabet, in lower
- * Return : Always return 0
+ * print_alphabet_x10 - Prints 10 times the alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
  */
+
 void print_alphabet_x10(void)
 {
 	char ch;
@@ -10,16 +13,18 @@ void print_alphabet_x10(void)
 
 	i = 0;
 
-	while {i < 10}
 	while (i < 10)
 	{
 		ch = 'a';
+
 		while (ch <= 'z')
 		{
 			_putchar(ch);
 			ch++;
 		}
+
 		_putchar('\n');
 		i++;
 	}
 }
+
