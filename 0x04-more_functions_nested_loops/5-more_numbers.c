3d27f0a247ab8d87db81168a54895f132db0f983
MavundlaBK <mavundlabj@gmail.com>
2023-03-25 05:38:20 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,37 +1,28 @@
 #include "main.h"
+
 /**
- * print_triangle - prints a triangle of size size
- * @size: size of triangle
+ * more_numbers - entry point
+ *
+ * Description: Prints the numbers with _putchar
  *
  * Return: void
  */
-void print_triangle(int size)
+
+void more_numbers(void)
 {
-	int i, j, space;
-	if (size <= 0)
+	int number, tens, units, row;
+
+	for (row = 1; row <= 10; row++)
 	{
+		for (number = 0; number <= 14; number++)
+		{
+			tens = number / 10;
+			units = number % 10;
+			if (number > 9)
+				_putchar(tens + '0');
+
+			_putchar(units + '0');
+		}
 		_putchar('\n');
 	}
-	else
-	{
-		for (i = 1; i <= size; i++)
-		{
-			for (space = 1; space <= (size - i; space++)
-			for (space = 1; space <= (size - i); space++)
-			{
-				_putchar(' ');
-			}
-			for (j = 1; j <= i; j++)
-			{
-				_putchar('#');
-			}
-			_putchar('\n');
-		}
-		_putchar(' ');
-	}
-	for (j = 1; j <= i; j++)
-	{
-		_putchar('#');
-	}
-	_putchar('\n');
 }
