28646e30aedd26b65aab3619737a80aa11e8a22c
BK <mavundlabj@gmail.com>
2023-05-28 12:29:08 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,13 +3,14 @@
 #include <unistd.h>
 
 /** main - prints a string without using printf
- 
- * Return: always 1
- 
- */
+* 
+* Return: always 1
+* 
+*/
 
 int main(void)
 {
-	write(1,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write (1,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	
 	return(1);
 }
